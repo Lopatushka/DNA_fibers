0d08479469c043070397f127a8526ccc039f1231
Lopatushka <helena.lopatukhina@gmail.com>
2026-07-09 21:51:30 +0200

    fix?

--- semi_manual_measure.py
+++ semi_manual_measure.py
@@ -3,6 +3,29 @@ from ij.gui import WaitForUserDialog, GenericDialog
 from ij.measure import ResultsTable
 from ij.plugin.frame import RoiManager
 import os
+# ---------------------------------
+# HELPERS
+# ---------------------------------
+def clear_results_and_rois():
+    IJ.run("Clear Results")
+
+    rm = RoiManager.getInstance()
+    if rm is not None:
+        rm.reset()
+
+def close_image(imp):
+    if imp is not None:
+        imp.close()
+        
+def close_all_images():
+    for wid in WindowManager.getIDList() or []:
+        imp = WindowManager.getImage(wid)
+        if imp is not None:
+            imp.close()
+            
+def safe_name(text):
+    return text.replace(" ", "_").lower()
+
 
 # ---------------------------------
 # MAIN FUNCTION
@@ -22,103 +45,99 @@ def main():
         
         path_1 = None
         path_2 = None
-
+        
+        # ---------------------------------
+        # Find the two images
+        # ---------------------------------
         for filename in files:
+            filename_low = filename.lower()
             
-            if  substring_1.lower() in filename.lower():
+            if substring_1 in filename_low:
                 path_1 = os.path.join(root, filename)
                 
-            if substring_2.lower() in filename.lower():
+            elif substring_2 in filename_low:
                 path_2 = os.path.join(root, filename)
             
-            # process only folders where both files were found
-            if path_1 is None or path_2 is None:
-                continue
+        # process only folders where both files were found
+        if path_1 is None or path_2 is None:
+            continue
 
-            IJ.log("Opening files: {} and {}".format(path_1, path_2))
+        IJ.log("Opening files: {} and {}".format(path_1, path_2))
 
-            imp_1 = IJ.openImage(path_1)
-            imp_2 = IJ.openImage(path_2)
+        imp_1 = IJ.openImage(path_1)
+        imp_2 = IJ.openImage(path_2)
             
-            if imp_1 is not None:
-                imp_1.show()
-            if imp_2 is not None:
-                imp_2.show()
+        if imp_1 is None or imp_2 is None:
+            IJ.log("Cannot open images.")
+            continue
+        
+        imp_1.show()
+        imp_2.show()
+        
+        # ---------------------------------
+        # Measurements
+        # ---------------------------------
                 
-            # Repeat measurements for the same image pair
-            count = 0
-            while count < 2:
-
-                # Clear previous measurements in Results table
-                IJ.run("Clear Results")
-                
-                # Clear ROI Manager
-                rm = RoiManager.getInstance()
-                if rm is not None:
-                    rm.reset()
+        count = 0
             
-                # Ask what will be measured
-                gd = GenericDialog("Measurement type")
-                gd.addChoice(
+        while count < 2:
+            clear_results_and_rois()
+
+            # Ask what will be measured
+            gd = GenericDialog("Measurement type")
+            gd.addChoice(
                     "Measure:",
                     ["Fiber length", "Interorigin distance", "Next", "Stop analysis"],
                     "Fiber length"
-                )
+            )
+            gd.showDialog()
 
-                gd.showDialog()
+            if gd.wasCanceled():
+                measurement_type = "Next"
+            else:
+                measurement_type = gd.getNextChoice()
 
-                if gd.wasCanceled():
-                    measurement_type = "Next"
-                else:
-                    measurement_type = gd.getNextChoice()
-
-                # Skip pair and go to next folder
-                if measurement_type == "Next":
-                    IJ.log("Moving to next image pair: {}".format(root))
-                    break
+            # Skip pair and go to next folder
+            if measurement_type == "Next":
+                IJ.log("Moving to next image pair: {}".format(root))
+                break
             
-                # Stop analysis
-                elif measurement_type == "Stop analysis":
-                    IJ.log("Analysis stopped by user.")
-                    # Close all opened images
-                    for wid in WindowManager.getIDList() or []:
-                        WindowManager.getImage(wid).close()
-                    return
+            # Stop analysis
+            elif measurement_type == "Stop analysis":
+                IJ.log("Analysis stopped by user.")
+                close_all_images()
+                return
             
-                # Wait for user
-                else:
-                    count += 1
-                    WaitForUserDialog(
-                        "Manual measurements",
-                        "Measure: {}\n\n"
-                        "Click OK to save these results.\n"
-                        "You will then be asked again for another measurement type.".format(
-                            measurement_type
-                        )
-                    ).show()
-            
-                    # Save Results table
-                    rt = ResultsTable.getResultsTable()
+            count += 1
                 
-                    if rt.size() > 0:
-                        folder_name = os.path.basename(root)
-                        save_path = os.path.join(root, folder_name + "_" + measurement_type + ".csv")
+            WaitForUserDialog(
+                    "Manual measurements",
+                    "Measure: {}\n\n"
+                    "Click OK to save these results.\n"
+                    "You will then be asked again for another measurement type.".format(
+                        measurement_type
+                    )
+            ).show()
+            
+            # Save Results table
+            rt = ResultsTable.getResultsTable()
+                
+            if rt.size() > 0:
+                folder_name = os.path.basename(root)
+                save_path = os.path.join(root, folder_name + "_" + measurement_type + ".csv")
 
-                        rt.save(save_path)
-                        IJ.log("Saved {} measurements: {}".format(measurement_type, save_path))
+                rt.save(save_path)
+                IJ.log("Saved {} measurements: {}".format(measurement_type, save_path))
                         
-                    else:
-                        IJ.log("No measurements made for {} in {}".format(
-                            measurement_type,
-                            root
-                        ))
+            else:
+                IJ.log("No measurements made for {} in {}".format(
+                    measurement_type,
+                    root
+                ))
 
         # Close images only after user skips/cancels this image pair
-            if imp_1 is not None:
-                imp_1.close()
-
-            if imp_2 is not None:
-                imp_2.close()
+        close_image(imp_1)
+        close_image(imp_2)
 
 if __name__ == "__main__":
     main()
