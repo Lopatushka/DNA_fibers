5230daad3e4e7af67cd3bb78e09d0660216472db
Lopatushka <helena.lopatukhina@gmail.com>
2026-07-09 20:32:56 +0200

    create opener.py file

---
+++ opener.py
@@ -0,0 +1,10 @@
+from ij import IJ, WindowManager
+import os
+
+# ---------------------------------
+# MAIN FUNCTION
+# ---------------------------------
+def main():
+    
+if __name__ == "__main__":
+    main()
